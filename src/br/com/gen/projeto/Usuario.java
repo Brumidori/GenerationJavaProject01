48afa49cc004d3605f1bf66b22de72811c33add5
Cleiton-Guilherme <Cleiton.guilherme@yahoo.com.br>
2022-07-16 20:44:47 -0300

    larT

---
+++ src/br/com/gen/projeto/Usuario.java
@@ -0,0 +1,35 @@
+package br.com.gen.projeto;
+
+public class Usuario {
+	
+	private String nomeUsuario;
+ 	private String tipoAnimal;
+    private String localSeguro;
+    
+
+	public String getNomeUsuario() {
+		return nomeUsuario;
+	}
+
+	public void setNomeUsuario(String nomeUsuario) {
+		this.nomeUsuario = nomeUsuario;
+	}
+
+	public String getTipoAnimal() {
+		return tipoAnimal;
+	}
+
+	public void setTipoAnimal(String tipoAnimal) {
+		this.tipoAnimal = tipoAnimal;
+	}
+
+	public String getLocalSeguro() {
+		return localSeguro;
+	}
+
+	public void setLocalSeguro(String localSeguro) {
+		this.localSeguro = localSeguro;
+	}
+ 	
+	
+	}
