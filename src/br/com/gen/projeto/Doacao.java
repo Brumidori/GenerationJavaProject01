fa645c4c462287024b292955afd290cdaf0af58c
Bruna_Midori <brunamidoriyy@yahoo.com.br>
2022-07-15 21:03:52 -0300

    Codando em switch

--- src/br/com/gen/projeto/Doacao.java
+++ src/br/com/gen/projeto/Doacao.java
@@ -19,21 +19,21 @@ public void setTipoDoacao(int tipoDoacao) {
     static String tipo (int b) {
     	String resposta = "";
     	
-	if (b != 1 && b != 2 && b != 3 ) {
+    	if (b != 1 && b != 2 && b != 3 ) {
 		resposta = "Número inválido. Por favor, reinice o programa.";
-	}
-	
-	if (b == 1) {
-	 resposta = "\nToda forma de contribuir é válida, temos certeza que seu brinquedo fará a vida de um Pet mais feliz." + "\nPara entregar o brinquedo visite nossa sede no endereço X, das 8h00 ãs 17h00";
-	} else if (b == 2) {
-	 resposta = "\nTodos precisam se alimentar e temos certeza que os Pets ficarão muito felizes com esta refeição!" + "\nPara entregar a ração visite nossa sede no endereço X, das 8h00 às 17h00";
-	} else if (b == 3){
-	resposta = "\nSua contribuição é muito importante para a manutenção do nosso trabalho!" 
-			+  "\nPara doar você pode fazer uma transferência para a nossa conta bancária ou pix no QR ao fim da página:"
-			+ "\n" +"\nDados Bancários: \nC.C. 0000001 \nAgência 00001 \nTitular: Lar Para Todos";
-	}
+		}
+		
+		if (b == 1) {
+			resposta = "\nToda forma de contribuir é válida, temos certeza que seu brinquedo fará a vida de um Pet mais feliz." + "\nPara entregar o brinquedo visite nossa sede no endereço X, das 8h00 ãs 17h00";
+		} else if (b == 2) {
+			resposta = "\nTodos precisam se alimentar e temos certeza que os Pets ficarão muito felizes com esta refeição!" + "\nPara entregar a ração visite nossa sede no endereço X, das 8h00 às 17h00";
+		} else if (b == 3){
+			resposta = "\nSua contribuição é muito importante para a manutenção do nosso trabalho!" 
+					+  "\nPara doar você pode fazer uma transferência para a nossa conta bancária ou pix no QR ao fim da página:"
+					+ "\n" +"\nDados Bancários: \nC.C. 0000001 \nAgência 00001 \nTitular: Lar Para Todos";
+		}
 		 
-	return resposta; 
+		return resposta; 
 		 
     }  	
 }
--- src/br/com/gen/projeto/larParaTodos.java
+++ src/br/com/gen/projeto/larParaTodos.java
@@ -5,55 +5,66 @@ public class larParaTodos {
 
 	public static void main(String[] args) {
 		
-		int contribuicao = 0, animal = 0 ;
-		Scanner input = new Scanner(System.in);
+		int t = 1;
 		
-		System.out.println("===================================================================");
-		System.out.println("=                       LAR PARA TODOS                /)-/)       =");
-		System.out.println("=                 Seu novo companheirinho esta aqui  (>^.^<)      =");
-		System.out.println("===================================================================");
-		
-	 
-		
-		System.out.println("\nComo vc gostaria de contribuir? \nDigite 1 - adoção; 2 - doação; 3 - denúncia." );
-		
-		
-		contribuicao = input.nextInt(); 
-		if ( contribuicao == 1) {
-			//adocao gato e cachorro
-			//classe ADO�ÇO ---> 
-			//CLASSE CADASTRO ---> Cadastro(); --- > roda a classe cadastro
+		do {
+			int contribuicao = 0, animal = 0 ;
+			Scanner input = new Scanner(System.in);
 			
-			System.out.println("Você gostaria de adotar um ? Digite 1 - dog;  2 - cat.");
-			 animal = input.nextInt(); 
-			 if (animal == 1) {
-				 //Classe dogs ---> Cachorro();
+			System.out.println("\n===================================================================");
+			System.out.println("=                       LAR PARA TODOS                /)-/)       =");
+			System.out.println("=                 Seu novo companheirinho esta aqui  (>^.^<)      =");
+			System.out.println("===================================================================");
+			
+		 
+			
+			System.out.println("\nComo vc gostaria de contribuir? \nDigite 1 - adoção; 2 - doação; 3 - denúncia; 0 - sair." );
+			
+			
+			contribuicao = input.nextInt(); 
+			switch ( contribuicao) {
+			case 1:
+				//adocao gato e cachorro
+				//classe ADO�ÇO ---> 
+				//CLASSE CADASTRO ---> Cadastro(); --- > roda a classe cadastro
+				
+				System.out.println("Você gostaria de adotar um ? Digite 1 - dog;  2 - cat.");
+				 animal = input.nextInt(); 
+				 if (animal == 1) {
+					 //Classe dogs ---> Cachorro();
+					 
+				 } else {
+					 //CLASSE GATO --> Gato();			 }
+				 }
 				 
-			 } else {
-				 //CLASSE GATO --> Gato();			 }
-			 }
-			
-		} else if (contribuicao == 2) {
-			//doa��o
-			// CLASSE DOA�ÇO ---> METODOS
-			
-			Doacao d1 = new Doacao();
+				 break;
+				
+			case 2: // inicio laco doacao
+				//cria objeto d1
+				
+				Doacao d1 = new Doacao();
+				
+				//imprime pergunta forma contribuir
+	            System.out.println("\nDe que forma você gostaria de contribuir? \nDigite 1 - Brinquedos; 2 - Ração; 3 - Dinheiro: ");
+	            d1.setTipoDoacao(input.nextInt()); //utiliza o Scanner e setter para inputar dado Inteiro
 
-            System.out.println("\nDe que forma você gostaria de contribuir? \nDigite 1 - Brinquedos; 2 - Ração; 3 - Dinheiro: ");
-            d1.setTipoDoacao(input.nextInt());
+	            int i = (Integer) d1.getTipoDoacao(); // transforma inteiro em String (método é String)
 
-            int i = (Integer) d1.getTipoDoacao();
-
-            System.out.println(Doacao.tipo(i));
+	            System.out.println(Doacao.tipo(i)); // imprime return da Classe Doacao método 'tipo'
+	            
+				break;
+				
+			case 3:
+				//denuncia
+				
+				//CLASSE DENUNCIA
+				// cai em uma mensagem com informa��es pra denuncia
+				break;
+			default:
+				t =0;
+			}
 			
-		}
-		
-		else {
-			//denuncia
-			
-			//CLASSE DENUNCIA
-			// cai em uma mensagem com informa��es pra denuncia 
-		}
+		} while (t != 0);
 		
 		   
 	}
